49fd11a46c3be179905b74048a96011d0004882f
Alexxmfs <alexxmfs@hotmail.com>
2023-01-04 16:10:38 -0300

    test input

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>VOCÊ ESTÁ LOGADO!</h1>
+      <h1>VOCÊ ESTÁ LOGADO!</h1>
+      <h1>VOCÊ ESTÁ LOGADO!</h1>
+      <h1>VOCÊ ESTÁ LOGADO!</h1>
+      <h1>VOCÊ ESTÁ LOGADO!</h1>
+    </div>
+  )
+}
+
+export default Home
